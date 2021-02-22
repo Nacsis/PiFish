7c1c2f8011e4e07bdef5bbf2a21ad16319211f9f
Nacsis <nacsis@posteo.de>
2021-02-22 19:04:25 +0100

    Motor test

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 
 import RPi.GPIO as GPIO
 import time
+from adafruit_motorkit import MotorKit
+
+
+kit = MotorKit()
 
 pin1=5
 pin2=6
+
+pin1_old = False
+pin2_old = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,16 +21,24 @@ i=0
 
 try:
     while True:
-        if GPIO.input(pin1):
-            print "Pin1 ON"
-        if GPIO.input(pin2):
-            print "Pin2 ON"
 
-	if not GPIO.input(pin1) and not GPIO.input(pin2):
-		print(i)
+	# Pin 1
+        if GPIO.input(pin1) and not pin1_old:
+		print("ON")
+		kit.motor1.throttle = 1
+		pin1_old = True
+		time.sleep(0.1)
+
+	if not GPIO.input(pin1) and pin1_old:
+		print("OFF")
+		kit.motor1.throttle = -1
+		pin1_old = False
+		time.sleep(0.1)
+
+	kit.motor1.throttle = 0
+
 
 	time.sleep(0.5)
-
 finally:
     GPIO.cleanup()
 
