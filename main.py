3f2605182915c82211b089a6ee82bccbec410ab1
Nacsis <nacsis@posteo.de>
2021-02-26 16:13:26 +0100

    current state

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #25 Input Recording
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #5 Button Even (pulled up)
+# GPIO.setup(5, GPIO.IN)
 #6 Sound sensor
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 i=0
@@ -26,21 +27,27 @@ try:
 	while True:
 		# HEAD
 		if GPIO.input(23):
+			#print(".")
 			kit.motor1.throttle = 1
 		else:
+			#print(".")
 			kit.motor1.throttle = 0
 
 		# Tail
 		if GPIO.input(25):
+			#print(".")
 			kit.motor2.throttle = 1
 		else:
+			print(".")
 			kit.motor2.throttle = 0
 
 		# Mouth
-		if GPIO.input(6):
+		if not GPIO.input(6):
 			kit.motor3.throttle = 1
+			time.sleep(0.10)
 		else:
 			kit.motor3.throttle = -1
+			time.sleep(0.00)
 
 		time.sleep(0.001)
 
